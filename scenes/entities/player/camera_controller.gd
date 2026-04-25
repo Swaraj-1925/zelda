6656431388fd9908ae88be0f0cf360a652d27399
Swaraj Gaikwad <swarajgaikwad1925@gmail.com>
2026-04-26 00:51:28 +0530

    Movement with camera, Jump,
    Additional changes:
     - switched from node3d to spring arm
     - moved logic refactoring

--- scenes/entities/player/camera_controller.gd
+++ scenes/entities/player/camera_controller.gd
@@ -1,13 +1,19 @@
 extends Node3D
 
-@export var mouse_sensitivity := 0.005
+@export var mouse_accleartion := 0.005
+#@export var horizontal_accleartion := 0.005
+#@export var vertical_accleartion := 0.005
+@export var min_limit_x := -0.8
+@export var max_limit_x := -0.2
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		rotate_from_vector(event.relative  * mouse_sensitivity)
+		rotate_from_vector(event.relative  * mouse_accleartion)
 		
 
 func rotate_from_vector(v:Vector2):
 	if v.length() == 0: return
-	print("rotaion",v.x)
-	rotation.y += v.x
+	rotation.y -= v.x
+	rotation.x -= v.y
+	rotation.x = clamp(rotation.x, min_limit_x, max_limit_x)
+	
--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -1,10 +1,41 @@
 extends CharacterBody3D
 
-@export var base_speed := 4.0
+@export var jump_height : float = 2.25
+@export var jump_time_to_peak : float = 0.4
+@export var jump_time_to_descent : float = 0.3
 
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+
+@export var base_speed := 8.0
+@onready var camera := $CameraController/Camera3D
 var movment_input := Vector2.ZERO
 
-func  _physics_process(_delta: float) -> void:
-	movment_input = Input.get_vector("a","d","w","s")
-	velocity = Vector3(movment_input.x, 0, movment_input.y) * base_speed
+func  _physics_process(delta: float) -> void:
+	
+	move_logic(delta)
+	jump_logic(delta)
 	move_and_slide()
+	
+func move_logic(delta: float):
+	movment_input = Input.get_vector("a","d","w","s")
+	movment_input = movment_input.rotated(-camera.global_rotation.y)
+	var vel = Vector2(velocity.x, velocity.z)
+	
+	# if player is moving/movment is presses the slowly increse the player speed to max base speed
+	if movment_input != Vector2.ZERO:
+		vel += movment_input * base_speed * delta
+		vel = vel.limit_length(base_speed)
+	else:
+		# if player has stopped moving then we slowy stop them with this
+		vel = vel.move_toward(Vector2.ZERO,base_speed * 4.0 * delta)
+	velocity.x = vel.x
+	velocity.z = vel.y
+func jump_logic(delta: float):
+	# this if statment make sure that no jump allowed when in the air can be modifed to be used for something like double jump
+	if is_on_floor(): 
+		if Input.is_action_just_pressed("space"):
+			velocity.y = -jump_velocity
+	var gravity = jump_gravity if velocity.y > 0.0 else fall_gravity
+	velocity.y -= gravity * delta
